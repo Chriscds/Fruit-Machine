b32753f4ce9b6ddbe1fdb27c27258fd409b860c0
CDS-admin1 <csouthon44@gmail.com>
2023-08-13 16:47:47 +0100

    Adding win conditions

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const   icon_width = 79,
-        icon_height = 79,
+const debugEl = document.getElementById('debug'),
+
+    icon_width = 79,
+    icon_height = 79,
         // number of icons in the reel
-        num_icons = 9,
-        time_per_icon = 100,
+    num_icons = 9,
+    time_per_icon = 100,
         // array of icons indexes
-        indexes = [0, 0, 0];
+    indexes = [0, 0, 0],
+    iconMap = ["Banana", "Seven","Cherries", "Plum", "Orange", "Bell", "Bar", "Lemon", "Melon"];
 
         // function to animate reel that can be repeated over the three reels
 const roll = (reel, offset = 0) => {
@@ -13,37 +16,47 @@ const roll = (reel, offset = 0) => {
         // (offset + 2) adds on to each reel
     const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons);
     const style = getComputedStyle(reel),
-            backgroundPositionY = parseFloat(style["background-position-y"]);
+            backgroundPositionY = parseFloat(style["background-position-y"]),
+            targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
+            normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
 
     return new Promise((resolve, reject) => {
 
-            reel.style.transition = `background-position-y ${8 +delta * time_per_icon}ms`;
+            reel.style.transition = `background-position-y ${8 +delta * time_per_icon}ms cubic-bezier(.45,.05,.58,1.09)`;
             reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`
 
             setTimeout(() => {
+                reel.style.transition = `none`;
+                reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
                 resolve(delta%num_icons)
         }, 8 + delta * time_per_icon)
     })
 };
 
 function rollAll() {
+
+    debugEl.textContent = 'rolling...';
+
     const reelsList = document.querySelectorAll('.slots > .reel');
 
     Promise
         .all( [...reelsList].map((reel, i) => roll(reel, i)) )
         .then((deltas) => {
-            console.log(deltas)
+            deltas.forEach((delta, i) => indexes[i] =(indexes[i] + delta)%num_icons);
+            debugEl.textContent = indexes.map((i) => iconMap[i]).join(' - ');
+        //    indexes.map((index) => {console.log(iconMap[index]) })
 
-            // check win conditions
+            			// Win conditions
+			if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
+				const winCls = indexes[0] == indexes[2] ? "win2" : "win1";
+				document.querySelector(".slots").classList.add(winCls);
+				setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
+			}
+		
+			// Again!
+			setTimeout(rollAll, 3000);
+		});
+};
 
-            setTimeout(rollAll, 3000);
-        })
-
-    // spread operator turning reelsList into an array, so we can map over the list
-    // [ ...reelsList].map((reel, i) => {
-    //     console.log(reel, i);
-    //     roll(reel, i).then((delta) => { console.log(delta) })
-    // })
-}
-
-rollAll();
+// Kickoff
+setTimeout(rollAll, 1000);
